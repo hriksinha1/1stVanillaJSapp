b9bc5444253aa920dd6e169f49bfb11d95d29bee
Hrik Sinha <dailyhrik@gmail.com>
2023-08-30 14:39:51 +0530

    final commit

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/pirate.json";
 
-console.log(txtInput)
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+// function errorHandler (error) {
+//     console.log("error occured", error);
+//     alert("something wrong with server try again after sometime")
+// }
 
 function clickHandler()  {
- outputDiv.innerText = txtInput.value ;
+//  outputDiv.innerText = txtInput.value ;
+var inputText = txtInput.value;
 
+
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+})
+// .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
